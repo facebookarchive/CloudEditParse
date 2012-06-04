e139be1ab02ec75aaadaad5d4db6f1a8d1e3fd8e
James Yu <jamesjacobyu@gmail.com>
2012-06-04 09:35:01 -0700

    update to latest backbone and put in more user friendly permission errors

--- js/views/edit.js
+++ js/views/edit.js
@@ -6,6 +6,7 @@ App.Views.Edit = Backbone.View.extend({
   initialize: function() {
     _.bindAll(this, 'render');
     this.model.bind('change', this.render);
+    this.editable = this.model.getACL().getWriteAccess(Parse.User.current().id);
     this.render();
   },
   
@@ -20,10 +21,14 @@ App.Views.Edit = Backbone.View.extend({
     this.model.save({ title: this.$('[name=title]').val(), body: this.$('[name=body]').val(), ACL: ACL }, {
       success: function(model, resp) {
         new App.Views.Notice({ message: msg });
-        Backbone.history.saveLocation('documents/' + model.id);
+        Backbone.history.navigate('documents/' + model.id);
       },
-      error: function() {
-        new App.Views.Error();
+      error: function(model, error) {
+        if (error.code === Parse.Error.OBJECT_NOT_FOUND) {
+          new App.Views.Notice({ message: "You don't have permission to edit this document!" });
+        } else {
+          new App.Views.Error();
+        }
       }
     });
     
@@ -31,8 +36,13 @@ App.Views.Edit = Backbone.View.extend({
   },
   
   render: function() {
-    $(this.el).html(_.template($("#document-template").html())({ model: this.model }));
+    $(this.el).html(_.template($("#document-template").html())({ model: this.model, editable: this.editable }));
     $('#app').html(this.el);
+
+    if (!this.editable) {
+      this.$("input").attr("disabled", "disabled");
+      this.$("textarea").attr("disabled", "disabled");
+    }
     
     // use val to fill in title, for security reasons
     this.$('[name=title]').val(this.model.get('title'));
